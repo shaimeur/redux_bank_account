6a8a7d76d7fb83a3dd5eb217a11e3e81f3edf0c5
shaimeur <haimeursaad@gmail.com>
2024-06-07 13:04:28 +0100

    adding the action creators to make our code more resuable

--- src/store.js
+++ src/store.js
@@ -49,14 +49,37 @@ function reducer (state=initialState,action){
 
 const store = createStore(reducer)
 
-store.dispatch({type : "account/deposit" ,payload : 500})
-console.log(store.getState())
-store.dispatch({type : "account/withdraw",payload : 200})
-console.log(store.getState())
-store.dispatch({type :"account/requestLoan",payload : {amount :2000000,purpose :"Buy a car"}})
-console.log(store.getState())
-store.dispatch({type:"account/payLoan"})
+// store.dispatch({type : "account/deposit" ,payload : 500})
+// console.log(store.getState())
+// store.dispatch({type : "account/withdraw",payload : 200})
+// console.log(store.getState())
+// store.dispatch({type :"account/requestLoan",payload : {amount :2000000,purpose :"Buy a car"}})
+// console.log(store.getState())
+// store.dispatch({type:"account/payLoan"})
+// console.log(store.getState())
 
+
+
+function deposit(amount){
+    return {type : "account/deposit" ,payload : amount}
+}
+console.log(store.dispatch(deposit(500)))
 console.log(store.getState())
 
-// export default store
+function withdraw(amount){
+    return {type : "account/withdraw",payload : amount}
+}
+console.log(store.dispatch(withdraw(200)))
+console.log(store.getState())
+function requestLoan(amount,purpose){
+    return {type :"account/requestLoan",payload : {amount ,purpose }}
+}
+console.log(store.dispatch(requestLoan(10000000,"buy an Iphone")))
+console.log(store.getState())
+function payLoan(){
+    return {type:"account/payLoan"}
+}
+console.log(store.dispatch(payLoan(200)))
+console.log(store.getState())
+
+// export default
